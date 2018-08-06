2f0dc295c97ad2b1093aca6344446991638a2ff7
daixiaohan <dxh@minilott.com>
2018-08-06 19:17:33 +0800

    AVLTree

---
+++ AVLTree/AVLTree.py
@@ -0,0 +1,241 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/8/6 18:26
+# @Author  : daixiaohan
+# @Email   : jasonxiaohan198@qq.com
+# @File    : AVLTree.py
+# @Software: PyCharm
+
+from DataStructure.FileOperation import FileOperation
+from DataStructure.Map.Map import Map
+
+"""
+使用二分搜索树实现map
+"""
+class AVLTree(Map):
+    class Node:
+        key = None
+        value = None
+        # 左子树、右子树
+        left = right = None
+
+        def __init__(self, key, value):
+            self.key = key
+            self.value = value
+            self.height = 1
+
+    root = None
+    size = 0
+    res = ""
+
+    def __init__(self):
+        self.size = 0
+
+    def getSize(self):
+        return self.size
+
+    def isEmpty(self):
+        return self.size == 0
+
+    def isBST(self):
+        """
+        判断该二叉树是否是一颗二分搜索树
+        :return:
+        """
+        keys = []
+        self.__inOrder(self.root, keys)
+
+        length = len(keys)
+        for i in range(1,length):
+            if keys[i-1] > keys[i]:
+                return False
+        return True
+
+    def __inOrder(self, node, keys):
+        """
+        中序遍历
+        :param keys:
+        :return:
+        """
+        if node == None:
+            return
+        self.__inOrder(node.left, keys)
+        keys.append(node.key)
+        self.__inOrder(node.right, keys)
+
+    def isBalanced(self):
+        """
+        判断该二叉树是否是一颗平衡二叉树
+        :return:
+        """
+        return self.__isBalanced(self.root)
+
+    def __isBalanced(self, node):
+        """
+        判断以Node为根的二叉树是否是一颗平衡二叉树，递归算法
+        :param node:
+        :return:
+        """
+        if node == None:
+            return True
+        balanceFactor = self.__getBalanceFactor(node)
+        if abs(balanceFactor) > 1:
+            return False
+        return self.__isBalanced(node.left) and self.__isBalanced(node.right)
+
+    def __getHeight(self, node):
+        """
+        获取节点node的高度
+        :param node:
+        :return:
+        """
+        if node == None:
+            return 0
+        return node.height
+
+    def __getBalanceFactor(self, node):
+        """
+        获取节点node的平衡因子
+        :param node:
+        :return:
+        """
+        if node == None:
+            return 0
+        return  int(self.__getHeight(node.left) - self.__getHeight(node.right))
+
+    def add(self, key, value):
+        self.root = self.__add(self.root, key, value)
+
+    def __add(self, node, key, value):
+        """
+        向node为根的二分搜索树中插入元素(k,v)，递归算法
+       返回插入新节点后二分搜索树的根
+        :param node:
+        :param key:
+        :param value:
+        :return:
+        """
+        if node == None:
+            self.size += 1
+            return self.Node(key, value)
+        if key < node.key:
+            node.left = self.__add(node.left, key, value)
+        elif key > node.key:
+            node.right = self.__add(node.right, key, value)
+        else: # key==node.key
+            node.value = value
+        # 更新height值
+        node.height = 1 + int(max(self.__getHeight(node.right), self.__getHeight(node.left)))
+        # 计算平衡因子
+        balanceFactor = self.__getBalanceFactor(node)
+        if abs(balanceFactor) > 1:
+            print("unbalanced："+str(balanceFactor))
+        return node
+
+    # 返回以node为根节点的二分搜索树，key所在的节点
+    def __getNode(self, node, key):
+        if(node == None):
+            return None
+        if(key == node.key):
+            return node
+        if(key < node.key):
+            return self.__getNode(node.left, key)
+        else:
+            return self.__getNode(node.right, key)
+
+    def contains(self, key):
+        return self.__getNode(self.root, key) != None
+
+    def get(self, key):
+        node = self.__getNode(self.root, key)
+        return node.value if node != None else None
+
+    def set(self, key, value):
+        node = self.__getNode(self.root, key)
+        if(node == None):
+            print(str(key)+" doesn't exist!")
+            return
+        node.value = value
+
+    """
+    寻找二分搜索树的最小元素
+    """
+    def minxmum(self):
+        return self.__minxmum(self.root).key
+
+    def __minxmum(self, node):
+        if (self.size == 0):
+            print("BST is empty.")
+            return
+        if (node.left == None):
+            return node
+        return self.__minxmum(node.left)
+
+    """
+   从二分搜索树中删除最小值所在节点，并返回最小值
+   """
+    def removeMin(self):
+        e = self.minxmum()
+        self.root = self.__removeMin(self.root)
+        return e
+
+    def __removeMin(self, node):
+        if (node.left == None):
+            rightNode = node.right
+            node.right = None
+            self.size -= 1
+            return rightNode
+        node.left = self.__removeMin(node.left)
+        return node
+
+    """
+    删除元素e
+    """
+    def remove(self, key):
+        node = self.__getNode(self.root, key)
+        if(node != None):
+            self.root = self.__remove(self.root, key)
+            return node
+        return None
+
+    def __remove(self, node, key):
+        if (node == None):
+            return None
+        if (key < node.key):
+            node.left = self.__remove(node.left, key)
+            return node
+        elif (key > node.key):
+            node.right = self.__remove(node.right, key)
+            return node
+        else:
+            if (node.left == None):
+                nodeRight = node.right
+                node.right = None
+                self.size -= 1
+                return nodeRight
+            if (node.right == None):
+                nodeLeft = node.left
+                node.left = None
+                self.size -= 1
+                return nodeLeft
+            # 否则存在左右两个子树
+            successor = self.__minxmum(node.right)
+            successor.right = self.__removeMin(node.right)
+            successor.left = node.left
+            node.left = node.right = None
+            return successor
+if __name__ == '__main__':
+    filename = '../pride-and-prejudice.txt'
+    words = FileOperation.readFile(filename)
+    print(len(words))
+    avl = AVLTree()
+    for word in words:
+        if(avl.contains(word)):
+            avl.set(word, int(avl.get(word))+1)
+        else:
+            avl.add(word, 1)
+    print("Total different words："+str(avl.getSize()))
+    print("Frequency of PRIDE："+str(avl.get("pride")))
+    print("Frequency of PREJUDICE："+str(avl.get("prejudice")))
+
+    print("is BST："+ str(avl.isBST()))
+    print("is Balanced："+ str(avl.isBalanced()))
---
+++ AVLTree/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/8/6 18:22
+# @Author  : daixiaohan
+# @Email   : jasonxiaohan198@qq.com
+# @File    : __init__.py.py
+# @Software: PyCharm
